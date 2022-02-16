fcdaa4bd689f9f50330bc4e6b518cd570fbe1e83
German Novikov <german.novikov@phystech.edu>
2022-02-16 23:14:15 +0300

    Dev (#1)
    * dev branch, no CI tests
    
    * update numpy version
    
    Co-authored-by: German Novikov <german.novikov@phystech.edu>

--- src/domicolor/domicolor.py
+++ src/domicolor/domicolor.py
@@ -1,1 +1,14 @@
 """Main module."""
+import numpy as np
+
+
+def color_histogram(image: np.ndarray) -> np.ndarray:
+    """Color histogram function.
+
+    Args:
+        image: lll
+
+    Returns:
+        histogram of the image
+    """
+    return np.histogram(image)[0]
--- tests/test_domicolor.py
+++ tests/test_domicolor.py
@@ -1,9 +1,11 @@
 """Tests for `domicolor` module."""
 from typing import Generator
 
+import numpy as np
 import pytest
 
 import domicolor
+from domicolor.domicolor import color_histogram
 
 
 @pytest.fixture
@@ -15,3 +17,11 @@ def version() -> Generator[str, None, None]:
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
     assert version == "0.1.0"
+
+
+def test_color_histogram() -> None:
+    """Test color_histogram function."""
+    image = np.zeros((100, 100, 3))
+    hist = color_histogram(image)
+    true_hist = [0, 0, 0, 0, 0, 30000, 0, 0, 0, 0]
+    assert np.all(hist == true_hist)
