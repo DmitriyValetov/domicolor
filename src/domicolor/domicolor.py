240e1b9013736ad215776ef7f0d2bfae3dc777ae
German Novikov <genovikov@sberbank.ru>
2022-02-16 20:45:58 +0300

    test commit

--- src/domicolor/domicolor.py
+++ src/domicolor/domicolor.py
@@ -1,1 +1,14 @@
 """Main module."""
+import numpy as np
+
+
+def color_histogram(image: np.array) -> np.array:
+    """Color histogram function.
+
+    Args:
+        image: lll
+
+    Returns:
+        histogram of the image
+    """
+    return np.histogram(image)[0]
--- tests/test_domicolor.py
+++ tests/test_domicolor.py
@@ -1,6 +1,7 @@
 """Tests for `domicolor` module."""
 from typing import Generator
 
+import numpy as np
 import pytest
 
 import domicolor
@@ -15,3 +16,11 @@ def version() -> Generator[str, None, None]:
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
     assert version == "0.1.0"
+
+
+def test_color_histogram(version: str) -> None:
+    """Test color_histogram function."""
+    image = np.zeros((10, 10, 3))
+    hist = domicolor.color_histogram(image)
+    true_hist = [0, 0, 0, 0, 0, 30000, 0, 0, 0, 0]
+    assert np.alltrue(hist == true_hist)
