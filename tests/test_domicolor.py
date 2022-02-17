00895bd038a1b71a0547ebb275b02ad8fe837348
German Novikov <genovikov@sberbank.ru>
2022-02-17 09:44:09 +0300

    fix version in tests

--- tests/test_domicolor.py
+++ tests/test_domicolor.py
@@ -16,7 +16,7 @@ def version() -> Generator[str, None, None]:
 
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
-    assert version == "0.1.1"
+    assert version == "0.1.2"
 
 
 def test_color_histogram() -> None:
