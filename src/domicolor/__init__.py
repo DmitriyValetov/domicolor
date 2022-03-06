543c00cbcd410becd8ab319204de7fb9c98b584d
DmitriyValetov <valetovdk@yandex.ru>
2022-03-07 02:00:53 +0300

    fill with code

--- src/domicolor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for domicolor."""
-
-__author__ = """Dmitriy Valetov"""
-__email__ = "dmitriy.valetov@phystech.edu"
-__version__ = "0.1.5"
--- tests/test_domicolor.py
+++ tests/test_domicolor.py
@@ -1,22 +1,24 @@
-"""Tests for `domicolor` module."""
-from typing import Generator
-
 import numpy as np
-import pytest
-
 import domicolor
-from domicolor.domicolor import color_histogram
 
 
-@pytest.fixture
-def version() -> Generator[str, None, None]:
-    """Sample pytest fixture."""
-    yield domicolor.__version__
+
+def test_get_palette():
+    test_img = np.ones((10,100,3))
+    test_img[:, :50, :] *= (255,0,0)
+    test_img[:, 50:80, :] *= (0,255,0)
+    test_img[:, 80:, :] *= (0,0,255)
+    test_img = test_img.astype('uint8')
+    colors_and_portions = domicolor.get_palette(test_img, 6)
+    assert len(colors_and_portions) == 3
+    
+    assert colors_and_portions[0][0] == (255, 0, 0)
+    assert abs(colors_and_portions[0][1]-0.5)<0.000001
+    
+    assert colors_and_portions[1][0] == (0, 255, 0)
+    assert abs(colors_and_portions[1][1]-0.3)<0.000001
+
+    assert colors_and_portions[2][0] == (0, 0, 255)
+    assert abs(colors_and_portions[2][1]-0.2)<0.000001
 
 
-def test_color_histogram() -> None:
-    """Test color_histogram function."""
-    image = np.zeros((100, 100, 3))
-    hist = color_histogram(image)
-    true_hist = [0, 0, 0, 0, 0, 30000, 0, 0, 0, 0]
-    assert np.all(hist == true_hist)
